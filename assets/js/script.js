d99c5020b0e41a032ac0caa455fb732e1f38cd8b
Mateus Daniel <mateusdaniiel@live.com>
2025-04-12 00:09:53 -0300

    feat: add insert player

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,39 @@
-function isMobile() {
-    const userAgentMatch = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-    const touchSupport = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+// verifica se o dispositivo é mobile ou desktop
+(() => {
+    function isMobile() {
+        const userAgentMatch = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+        const touchSupport = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+    
+        return userAgentMatch || touchSupport;
+    }
+    
+    if (isMobile()) {
+        setInterval(() => {
+            document.querySelector('.sec-modal-landscape').setAttribute('hidden', true);
+        }, 5000);   
+    }
+})();
 
-    return userAgentMatch || touchSupport;
-}
+// adiciona jogadores
+(() => {
+    'use strict';
 
-if (isMobile()) {
-    setInterval(() => {
-        document.querySelector('.sec-modal-landscape').setAttribute('hidden', true);
-    }, 5000);   
-}
+    const players = [];
+    
+    const inputPlayers = document.querySelector('.sec-add-players__content__form__input');
+    const btSubmit = document.querySelector('.sec-add-players__content__form__bt');
+
+    btSubmit.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        if (inputPlayers.value !== '') {
+            const getPlayers = inputPlayers.value.split(',');
+
+            getPlayers.forEach((el) => {
+                players.push(el);
+            });
+        }
+
+    });
+
+})();
